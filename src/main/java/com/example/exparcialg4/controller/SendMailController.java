1b811ae4f11d4cfff7c8f7a5c385cdc2a2be19d8
Sarita <atanacio.sarita@pucp.pe>
2020-06-07 19:56:09 -0500

    avance

--- src/main/java/com/example/exparcialg4/controller/SendMailController.java
+++ src/main/java/com/example/exparcialg4/controller/SendMailController.java
@@ -1,4 +1,4 @@
-package com.example.exparcialg4.service;
+package com.example.exparcialg4.controller;
 
 import com.example.exparcialg4.entity.Usuarios;
 import com.example.exparcialg4.repository.UsuarioRepository;
@@ -14,6 +14,7 @@ import java.util.Random;
 public class SendMailController {
     @Autowired
     UsuarioRepository usuarioRepository;
+
     @Autowired
     SendMailService sendMailService;
 
